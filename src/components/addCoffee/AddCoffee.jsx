6819d8f630e8773f5f9d37eae2cb4a5be43ddc96
Raihan Uddin <raihanarif778@gmail.com>
2025-01-21 12:59:53 +0600

    react-router & new component added

---
+++ src/components/addCoffee/AddCoffee.jsx
@@ -0,0 +1,11 @@
+
+
+const AddCoffee = () => {
+    return (
+        <div>
+            <h1>Add New Coffee</h1>
+        </div>
+    );
+};
+
+export default AddCoffee;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
+import AddCoffee from './components/addCoffee/AddCoffee.jsx'
+import UpdateCoffee from './components/updateCoffee/UpdateCoffee.jsx'
+import './index.css'
+
+const router=createBrowserRouter([
+  {
+    path:'/',
+    element:<App></App>,
+    children:[
+     
+
+    ]
+
+  },
+  {path:'/update-coffee',
+    element:<UpdateCoffee></UpdateCoffee>
+
+  },
+  { path:'/add-coffee',
+    element:<AddCoffee></AddCoffee>,
+
+  }
+
+
+])
+
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
